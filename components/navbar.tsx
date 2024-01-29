ef6eeb83250dd1fb4d3e2bb6b2e16918fdffb20b
Matheus <mat.alvessantana@gmail.com>
2024-01-29 16:25:45 -0300

    create dashboard page and add dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,24 +3,26 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () => {
-  const {userId} = auth()
-  if(!userId) {
-    redirect('/sign-in')
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
   }
   const stores = await prismadb.store.findMany({
     where: {
-      userId
-    }
-  })
+      userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores}/>
-        <MainNav className="mx-6"/>
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton afterSignOutUrl="/"/>
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
